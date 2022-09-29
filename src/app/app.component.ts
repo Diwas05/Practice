686a7b7e383c5cacb06121209a158e2ef77a4d26
ipsit <diwasmundra@gmail.com>
2022-09-29 18:46:49 +0530

    My first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'my-Title';
+  count=0;
+  number1=0;
+  number2=0;
+  result=0;
+  listofNumbers=[12,32,5,7,98,45];
+  arr=["Diwas","Raj","Pranish"];
+  increment = () => {
+    this.count++;
+  }
+  decrement = () => {
+    this.count--;
+  }
+  add = () => {
+    this.result=Number(this.number1)+Number(this.number2);
+  }
 }
